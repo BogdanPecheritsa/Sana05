5674db5d2aebd8a8b13a017fafa6b78e7ecda84c
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-27 12:03:06 +0200

    Added Airplane & MyDate classes with certain methods and protected fields

---
+++ Airplane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana05
+{
+    internal class Airplane
+    {
+        protected string StartCity;
+        protected string FinishCity;
+        protected MyDate StartDate;
+        protected MyDate FinishDate;
+
+        public void GetTotalTime()
+        {
+        }
+
+        public bool IsArrivingToday()
+        { 
+            return true; 
+        }
+    }
+}
+
---
+++ MyDate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana05
+{
+    internal class MyDate
+    {
+        protected int Year;
+        protected int Month;
+        protected int Day;
+        protected int Hours;
+        protected int Minutes;
+    }
+}
