cdfec3f729ef653db273df6d9eb24eb3d2ce9ae3
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-28 14:17:22 +0200

    Added Currency & Product classes with certain methods and protected fields

---
+++ Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana05
+{
+    internal class Product
+    {
+        protected string Name;
+        protected double Price;
+        protected Currency Cost;
+        protected int Quantity;
+        protected string Producer;
+        protected double Weight;
+    }
+    public double GetPriceInUAH()
+    {
+        return 2;
+    }
+}
